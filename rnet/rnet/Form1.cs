7fdd0a3a58c2db4dbffab530a72169f5b82f443b
mgtstrategies <mgt@ms-strategies.com>
2016-01-21 19:57:27 +0530

    initial commit

---
+++ rnet/rnet/Form1.cs
@@ -0,0 +1,41 @@
+﻿using RDotNet;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace rnet
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            REngine.SetEnvironmentVariables();
+            // There are several options to initialize the engine, but by default the following suffice:
+            REngine engine = REngine.GetInstance();
+
+            // .NET Framework array to R vector.
+            NumericVector group1 = engine.CreateNumericVector(new double[] { 30.02, 29.99, 30.11, 29.97, 30.01, 29.99 });
+            engine.SetSymbol("group1", group1);
+            // Direct parsing from R script.
+            NumericVector group2 = engine.Evaluate("group2 <- c(29.89, 29.93, 29.72, 29.98, 30.02, 29.98)").AsNumeric();
+
+            // Test difference of mean and get the P-value.
+            GenericVector testResult = engine.Evaluate("t.test(group1, group2)").AsList();
+            double p = testResult["p.value"].AsNumeric().First();
+
+            Console.WriteLine("Group1: [{0}]", string.Join(", ", group1));
+            Console.WriteLine("Group2: [{0}]", string.Join(", ", group2));
+            Console.WriteLine("P-value = {0:0.000}", p);
+
+            // you should always dispose of the REngine properly.
+            // After disposing of the engine, you cannot reinitialize nor reuse it
+            engine.Dispose();
+        }
+    }
+}
